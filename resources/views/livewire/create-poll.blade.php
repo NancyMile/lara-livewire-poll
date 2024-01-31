f6c0cb0dffc05647816fb2ec89a835673b2753a9
NancyMile <smithnancy@outlook.com>
2024-01-31 04:30:26 +0000

    eiting poll options

---
+++ resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <form>
+        <label">Poll Title</label>
+        <input type="text" wire:model.live="title">
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+
+        <div class="mb-4">
+            @foreach ( $options as $key => $option )
+                <div class="mb-4">
+                    <label>Option  {{ $key + 1}} </label>
+                    <div class="flex gap-2">
+                        <input type="text" wire.model="options.{{ $key }}" />
+                        <button class="btn" wire:click.prevent="removeOption({{ $key }})">Remove</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="mb-4 mt-4">
+            <button class="btn"
+            >Create Poll</button>
+        </div>
+
+
+    </form>
+</div>
